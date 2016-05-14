41e16c0977be6846523e4d5ac43d78358ea67037
Ravi <rxt140930@utdallas.edu>
2016-05-13 22:51:51 -0500

    Update AlgoType.java

--- Graphs--ShortestPathAlgorithms/AlgoType.java
+++ Graphs--ShortestPathAlgorithms/AlgoType.java
@@ -1,4 +1,10 @@
-
+/**
+ * Enum for graph type: based on the shortest path algorithm 
+ * applicable for that graph
+ * 
+* @author Ravindhar Reddy Thallapureddy
+* Last modified on : 5/13/2016 10:50 pm
+ */
 public enum AlgoType {
 	BFS,
 	DAG,
